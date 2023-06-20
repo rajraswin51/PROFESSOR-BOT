14b561773869532cbb5eb89d256ca472126bfa17
Dorafilms <129712623+rajraswin51@users.noreply.github.com>
2023-06-20 18:56:41 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -29,9 +29,9 @@ class script(object):
 
 ⪼ 𝐄𝐱𝐚𝐦𝐩𝐥𝐞 ›
 
-<code>/batch https://t.me/mkn_bots_updates https://t.me/mkn_bots_updates</code>
+<code>/batch https://t.me/olamfilmsbot https://t.me/olamfilmsbot</code>
 
-𝙲𝚁𝙴𝙳𝙸𝚃𝚂 ›› <a href=https://t.me/mkn_bots_updates><b>𝙼𝙺𝙽 𝙱𝙾𝚃𝚉</b></a>"""
+𝙲𝚁𝙴𝙳𝙸𝚃𝚂 ›› <a href=https://t.me/dorapublicationbot><b>dorapublicationbot</b></a>"""
     
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
